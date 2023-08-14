5916861bdd619720f6fd17b31f0c11f02c773e6b
aoi562227 <aoi562227@gmail.com>
2023-08-15 00:33:04 +0900

    create Springboot by init

---
+++ subject_eval/src/main/java/hufs_evaluation/subject_eval/SubjectEvalApplication.java
@@ -0,0 +1,13 @@
+package hufs_evaluation.subject_eval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubjectEvalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubjectEvalApplication.class, args);
+	}
+
+}
---
+++ subject_eval/src/test/java/hufs_evaluation/subject_eval/SubjectEvalApplicationTests.java
@@ -0,0 +1,13 @@
+package hufs_evaluation.subject_eval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubjectEvalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
