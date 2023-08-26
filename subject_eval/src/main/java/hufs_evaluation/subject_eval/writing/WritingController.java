f2dd0a5da35a36f8f898de9e415f52ba3a222a89
aoi562227 <aoi562227@gmail.com>
2023-08-26 12:54:19 +0900

    view 및 article 관련 BE controller

--- subject_eval/src/main/java/hufs_evaluation/subject_eval/writing/WritingController.java
+++ subject_eval/src/main/java/hufs_evaluation/subject_eval/writing/WritingController.java
@@ -3,10 +3,13 @@ package hufs_evaluation.subject_eval.writing;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 @Controller
 @Slf4j
@@ -25,6 +28,20 @@ public class WritingController {
     public String post(Article article) {
         writingRepository.save(article);
         return "redirect:";
+    }
+
+    @GetMapping("/view")
+    public String view(Model model) {
+        List<Article> articles = writingRepository.findAll();
+        model.addAttribute("articles", articles);
+        return "basic/view";
+    }
+
+    @GetMapping("/view/{id}")
+    public String article(@PathVariable long id, Model model) {
+        Article article = writingRepository.findById(id);
+        model.addAttribute("article", article);
+        return "basic/article";
     }
 
     @PostConstruct
