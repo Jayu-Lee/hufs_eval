f97cba7d1a57d4e062d487fe5229552957d22afb
Jayu-Lee <114810429+Jayu-Lee@users.noreply.github.com>
2023-11-02 23:58:30 +0900

    Update App.js
    라우터 수정

--- subject_eval/src/main/frontend/hufs/src/App.js
+++ subject_eval/src/main/frontend/hufs/src/App.js
@@ -1,30 +1,33 @@
 import logo from './logo.svg';
 import React from "react";
-import {Routes, Route, Link} from "react-router-dom"
+import {BrowserRouter as Routes, Route, Switch} from "react-router-dom"
 import './App.css';
 import Post from './components/Post.js';
 import View from './components/View.js';
 import Test from './components/Test.js';
+import Article from './components/Article.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-      <div className="App">
-          <nav>
-              <Link to="/post">Post</Link>
-              <br/>
-              <Link to="/view">View</Link>
-              <br/>
-              <Link to="/test">Test</Link>
-          </nav>
-          <Routes>
-              <Route path="/post" element={<Post />}/>
-              <Route path="/view" element={<View />}/>
-              <Route path="/test" element={<Test />}/>
-          </Routes>
-      </div>
+    <div className="App">
+      <nav>
+        <Link to="/post">Post</Link>
+        <br />
+        <Link to="/view">View</Link>
+        <br />
+        <Link to="/test">Test</Link>
+      </nav>
+      <Router>
+        <Switch>
+          <Route path="/post" exact component={Post} />
+          <Route path="/view" exact component={View} />
+          <Route path="/test" exact component={Test} />
+          <Route path="/article/:id" component={Article} />
+        </Switch>
+      </Router>
+    </div>
   );
-
 }
 
 export default App;
