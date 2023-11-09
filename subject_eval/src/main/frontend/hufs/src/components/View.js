413c230262574cacae18093629efda908af0c332
Jayu-Lee <114810429+Jayu-Lee@users.noreply.github.com>
2023-11-10 02:54:23 +0900

    1110 View.js
    목록에서 게시글 눌렀을 때 개별 상세 페이지 로딩
    삭제 기능 조금 수정 + 안 쓰는 파라미터 지우고 간소화

--- subject_eval/src/main/frontend/hufs/src/components/View.js
+++ subject_eval/src/main/frontend/hufs/src/components/View.js
@@ -2,16 +2,17 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-function View(props) {
-    const [articles, setArticles] = useState([]);
+function View() {
+    const {id} = useParams();
+    const [articles, setArticles] = useState({});
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        // Axios를 사용하여 데이터 가져오기
-        axios.get('/api/view') // 스프링 백엔드의 엔드포인트로 변경
+        // Axios를 사용하여 데이터 가져오기... props 안 쓰는 거 같아서 그냥 지웠어요
+        axios.get('/api/article/${id}') // 스프링 백엔드의 엔드포인트로 변경... 개별 게시글을 끌어오려면 이렇게 바꿔야 되지 않을까요?
             .then((response) => {
                 // 요청이 성공하면 데이터를 상태에 저장합니다.
-                setArticles(response.data.data); // 'data' 프로퍼티에 실제 데이터가 들어 있을 것으로 예상됩니다.
+                setArticles(response.data); // 'data' 프로퍼티에 실제 데이터가 들어 있을 것으로 예상됩니다.
                 setLoading(false);
             })
             .catch((error) => {
@@ -19,7 +20,13 @@ function View(props) {
                 console.error('데이터를 가져오는 중 오류 발생:', error);
                 setLoading(false);
             });
-    }, []);
+    }, []);   //컴포넌트 마운트될 때 단 1회만 빈 배열 전달 -> 실행
+
+    const handleDelete = (id) => {
+        axios.delete('/api/article/${id}').then()) => {
+            setArticle(articles.filter((article) => article.id !== id));
+        ]);
+    }; //게시글 삭제 후 바로 새 db 불러오게끔
 
     if (loading) {
         return <p>데이터를 로딩 중입니다...</p>;
@@ -46,7 +53,7 @@ function View(props) {
             </div>
 
             <hr className="my-4" />
-
+        
             <div>
                 <table className="table">
                     <thead>
