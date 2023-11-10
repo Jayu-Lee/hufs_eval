38a8fd5c1ca3e4b1b853210522f1adb23e05b118
Chahee Lee <aaa1685@naver.com>
2023-11-10 18:30:15 +0900

    add hikari setting, change /api endpoint

--- subject_eval/src/main/frontend/hufs/src/setProxy.js
+++ subject_eval/src/main/frontend/hufs/src/setProxy.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: dbEndpoint,	// 서버 URL or localhost:설정한포트번호
+     target: 'http://127.0.0.1:8080',	// 서버 URL or localhost:설정한포트번호
       changeOrigin: true,
     })
   );
